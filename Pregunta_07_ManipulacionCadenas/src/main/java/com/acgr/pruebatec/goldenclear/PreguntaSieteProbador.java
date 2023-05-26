214a63cf54837eacba2af0d5342da854908fed25
cristina20139 <52253057+cristina20139@users.noreply.github.com>
2023-05-26 11:01:57 -0500

    Ultimos ajustes Pregunta 7 y 8
    Ultimos Ajustes Pregunta 7 y 8 mensajes en consola mas claros

--- Pregunta_07_ManipulacionCadenas/src/main/java/com/acgr/pruebatec/goldenclear/PreguntaSieteProbador.java
+++ Pregunta_07_ManipulacionCadenas/src/main/java/com/acgr/pruebatec/goldenclear/PreguntaSieteProbador.java
@@ -5,18 +5,25 @@ public class PreguntaSieteProbador {
     public void probarPuntoSiete(){
         PreguntaSieteManipulacionCadenasManager manager = new PreguntaSieteManipulacionCadenasManager();
         String cadena="AuraLaGuerreraAmazona";
+        System.out.println("La cadena de caracteres en minúsculas");
         String resultado=manager.convertirAMinuscula(cadena);
         System.out.println(resultado);
+        System.out.println("La cadena de caracteres en mayúsculas ");
         resultado=manager.convertirAMayusucula(cadena);
         System.out.println(resultado);
+        System.out.println("Los 2 primeros caracteres de la cadena");
         resultado=manager.obtenerPrimerosDosDigitosCadena(cadena);
         System.out.println(resultado);
+        System.out.println("Los 2 últimos caracteres de la cadena");
         resultado=manager.obtenerUltimosDosDigitosCadena(cadena);
         System.out.println(resultado);
+        System.out.println("El número de ocurrencias en la cadena del último carácter");
         int num_ocurrencias=manager.obtenerOcurrenciasCadenaUltimoCaracter(cadena);
         System.out.println(num_ocurrencias);
+        System.out.println("La cadena con 3 asteriscos por delante y 3 asteriscos por detrás");
         resultado=manager.tresAsteriscosAdelanteyAtras(cadena);
         System.out.println(resultado);
+        System.out.println("La cadena invertida.");
         resultado=manager.revertirCadena(cadena);
         System.out.println(resultado);
     }
--- Pregunta_08_CuentasBancarias/src/main/java/com/acgr/pruebatec/goldenclear/ProbadorCuentasBancarias.java
+++ Pregunta_08_CuentasBancarias/src/main/java/com/acgr/pruebatec/goldenclear/ProbadorCuentasBancarias.java
@@ -21,9 +21,12 @@ public class ProbadorCuentasBancarias {
     private void probarCuentaCorriente(){
         CuentaCorriente cc = new CuentaCorriente("Aura","Garzon",15,"1234567890",1000000000);
         CuentaCorriente cc2 = new CuentaCorriente("1234567890",0);
+        System.out.println("Saldo inciial -> "+cc.getSaldo());
         cc.ingresar(200);
+        System.out.println("Saldo despues de consignar 200 -> "+nf1.format(cc.getSaldo()));
         System.out.println(nf1.format(cc.getSaldo()));
         cc.reintegro(400);
+        System.out.println("Saldo despues de retirar 400 -> "+nf1.format(cc.getSaldo()));
         System.out.println(nf1.format(cc.getSaldo()));
     }
 
@@ -35,8 +38,8 @@ public class ProbadorCuentasBancarias {
         CuentaAhorro ca1 = new CuentaAhorro("1234567890",0,0);
         CuentaAhorro ca2 = new CuentaAhorro("1234567890",12);
         System.out.println(nf1.format(ca2.calcularSaldoSegunInteres()));
-        System.out.println(ca1.concatenarNumeroCuentaSaldo());
-        System.out.println(ca2.concatenarNumeroCuentaSaldo());
+        System.out.println("Contatenacion ca1 cuenta y saldo -> "+ca1.concatenarNumeroCuentaSaldo());
+        System.out.println("Contatenacion ca1 cuenta y saldo -> "+ca2.concatenarNumeroCuentaSaldo());
 
     }
 
